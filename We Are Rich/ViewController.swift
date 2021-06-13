0e2cb8f5a2efa11348b5a39911fabe194433e97c
Jordan Ball <cjordanball@ballwebdev.com>
2021-06-13 14:27:13 -0400

    initial commit

---
+++ We Are Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  We Are Rich
+//
+//  Created by C. Jordan Ball III on 6/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // This app is completely without functionality, so nothing is happening here.
+    }
+
+
+}
+
